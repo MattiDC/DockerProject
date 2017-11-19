80fcf88309059aee5552ab2a1a583e60241b83f4
root <root@us.staff.khk.be>
2017-11-19 09:35:04 +0100

    first commit

---
+++ MySQL/startsql.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mysql -e "GRANT ALL ON *.* to root@'192.168.1.4' IDENTIFIED BY 'your-root-password'"
+
+mysql -e "FLUSH PRIVILEGES"
---
+++ MySQL/startsql.sql
@@ -0,0 +1,2 @@
+GRANT ALL ON *.* to root@'192.168.1.4' IDENTIFIED BY 'your-root-password';
+FLUSH PRIVILEGES;
