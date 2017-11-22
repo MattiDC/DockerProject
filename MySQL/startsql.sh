98cc38a6f3ec3eae03bc0b9c0a1006893f027f1e
MattiDC <33784762+MattiDC@users.noreply.github.com>
2017-11-22 11:07:21 +0100

    Update startsql.sh

--- MySQL/startsql.sh
+++ MySQL/startsql.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-mysql -u "root" -p"root"
+mysql -u "root" -p"root" -e "GRANT ALL ON *.* to root@'172.18.0.3' IDENTIFIED BY 'root'"
 
-mysql -e "GRANT ALL ON *.* to root@'172.18.0.3' IDENTIFIED BY 'root'"
-
-mysql -e "FLUSH PRIVILEGES"
+mysql -u "root" -p"root" -e "FLUSH PRIVILEGES"
